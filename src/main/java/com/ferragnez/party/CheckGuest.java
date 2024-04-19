a8e41a437f53f660a2766e28db29d342307eaeb5
RosaCielo <ashyzan@gmail.com>
2024-04-19 11:23:10 +0200

    Config project

---
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,13 @@
+package com.ferragnez.party;
+
+public class CheckGuest {
+	
+	public static void main(String[] args) {
+	
+		//creare e inizializzare l’array contenente i nomi degli invitati 
+		//chiedere all’utente come si chiama e  verificare che il nome sia presente nella lista; 
+		//lasciarlo entrare o rispedirlo cortesemente da dove è venuto
+		// Lista invitati: Dua Lipa, Paris Hilton, Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi, Bebe Vio, Luis, Pardis Zarei, Martina Maccherone, Rachel Zeilic
+	}
+
+}
